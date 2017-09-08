7fb4125df3383966be0136037cdce10c1358dd15
Marcos Vinícius Rodrigues <mvrpl@icloud.com>
2017-09-08 13:43:17 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,32 @@
 import mvrpl
 import json
 
-print "------------- Retorna DocString do modulo -------------"
-print mvrpl.__doc__
+print("------------- Retorna DocString do modulo -------------")
+print(mvrpl.__doc__)
 print
 
-print "------------- Retorna resultado Python -------------"
-print mvrpl.retpy()
+print("------------- Retorna resultado Python -------------")
+print(mvrpl.retpy())
 print
 
-print "------------- Retorna Mapa Python -------------"
+print("------------- Retorna Mapa Python -------------")
 for k,v in mvrpl.retmapa().iteritems():
-    print "Chave: %s" % k
-    print "Valor: %s" % v
+    print("Chave: %s" % k)
+    print("Valor: %s" % v)
     print
 
-print "------------- Retorna Lista Python -------------"
-print "__".join([str(n) for n in mvrpl.reclista([1,2,3])])
+print("------------- Retorna Lista Python -------------")
+print("__".join([str(n) for n in mvrpl.reclista([1,2,3])]))
 print
 
-print "------------- Retorna resposta de URL -------------"
-print mvrpl.returl()
+print("------------- Retorna resposta de URL -------------")
+print(mvrpl.returl())
 print
 
-print "------------- Retorna Mapa Rust no Python -------------"
-print mvrpl.mapa()
+print("------------- Retorna Mapa Rust no Python -------------")
+print(mvrpl.mapa())
 print
 
-print "------------- Retorna parse dict Python -------------"
-print mvrpl.recmapa({'key':15})
+print("------------- Retorna parse dict Python -------------")
+print(mvrpl.recmapa({'key':15, 'ds': 5}))
 print
